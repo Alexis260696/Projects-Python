b05e91021348c429dbe097ec414d0bab29894305
Alexis260696 <125107629+Alexis260696@users.noreply.github.com>
2023-06-08 20:02:55 -0600

    Actualizar area of circles and triangles.py

--- area of circles and triangles.py
+++ area of circles and triangles.py
@@ -1,16 +1,31 @@
-print("areas of circles and triangles")
-print("choose a geometric figure")
-print("C = circle", "T = triangle ")
-R = input("What figure do you want to calculate?")
-if R == "C"or R == "c":
-    r = float(input("what is the radius of the circle?"))
-    pi = 3.1416
-    print(pi * r**2)
-elif R == "T" or R == "t":
-    ca = int(input ("enter a number ca ") )
-    co = int(input ("enter a number co ") )
-    h2 = ca*ca + co*co 
-    h = h2 ** 0.5
-    print (h)
-else:
-    print ("chinga tu madre te pedi C o T no tus mamadas ")
+def circle_area_calculator(radius):
+	pi = 3.1416
+	result = pi * (radius ** 2)
+	return result
+	
+def triangle_area_calculator(base, height):
+	result = base * height / 2
+	return result
+	
+while True:
+	print("Area Of Circles And Triangles")
+	print("Choose A Geometric Figure")
+	print("[C]ircle\n[T]riangle\n[E]xit")
+	
+	answer = input("What Figure Do You Want To Calculate?: ")
+	
+	if answer == "C" or answer == "c":
+		radius = float(input("What Is The Radius Of The Circle?: "))
+		print("The Area Of ​​The Circle Is ", circle_area_calculator(radius))
+	
+	elif answer == "T" or answer == "t":
+		triangle_base = int(input ("Enter A Triangle Base: "))
+		tringle_height = int(input ("Enter A Triangle Height: ") )
+		print("The Area Of The Triangle Is ", triangle_area_calculator(triangle_base, tringle_height))
+		
+	elif answer == "E" or answer == "e":
+		break
+		
+	else:
+		print ("Enter A Valid Command")
+        
