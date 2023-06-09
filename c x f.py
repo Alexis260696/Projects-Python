44c5f06e7e7b0d5cf58c72dae552392752e6a9b5
Alexis260696 <125107629+Alexis260696@users.noreply.github.com>
2023-06-08 20:16:14 -0600

    Actualizar c x f.py

--- c x f.py
+++ c x f.py
@@ -1,3 +1,5 @@
-C = float(input('intro celsius degrees '))
-F = (C * 9.0/5.0 ) + 32 
-print('{} degrees C is {} degrees F '.format(C, F))
+print("Welcome To Celsius To Fahrenheit Converter")
+
+celsius = float(input('Intro Celsius Degrees: '))
+fahrenheit = (celsius * 9.0/5.0 ) + 32 
+print('{} Celsius Degrees Is\n{} Fahrenheit Degrees '.format(celsius, fahrenheit))
